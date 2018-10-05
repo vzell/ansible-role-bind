a432c0a8e7871d27665a4999e523308b1bf58a26
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2018-10-05 15:24:23 +0200

    Use more recent docker-tests script with fixed run options

--- docker-tests.sh
+++ docker-tests.sh
@@ -2,9 +2,23 @@
 #
 # Author: Bert Van Vreckem <bert.vanvreckem@gmail.com>
 #
-# Runs tests for this Ansible role on a Docker container
-# Environment variables DISTRIBUTION and VERSION must be set
-# See usage() for details.
+# Usage: DISTRIBUTION=<distro> VERSION=<version> ./docker-tests.sh
+#
+# Creates a Docker container for the specified Linux distribution and version,
+# available at https://hub.docker.com/r/bertvv/ansible-testing/; runs a syntax
+# check; applies this role to the container using a test playbook; and,
+# finally, runs an idempotence test.
+#
+# Environment variables DISTRIBUTION and VERSION must be set outside of the
+# script.
+#
+# EXAMPLES
+#
+# $ DISTRIBUTION=centos VERSION=7 docker-tests.sh
+# $ DISTRIBUTION=fedora VERSION=28 docker-tests.sh
+# $ DISTRIBUTION=debian VERSION=9 docker-tests.sh
+# $ DISTRIBUTION=ubuntu VERSION=18.04 docker-tests.sh
+#
 
 #{{{ Bash settings
 # abort on nonzero exitstatus
@@ -19,11 +33,18 @@ readonly script_name=$(basename "${0}")
 readonly script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 IFS=$'\t\n'   # Split on newlines and tabs (but not on spaces)
 
+# Color definitions
+readonly reset='\e[0m'
+readonly red='\e[0;31m'
+readonly yellow='\e[0;33m'
+
+# Test environment
 readonly container_id="$(mktemp)"
 readonly role_dir='/etc/ansible/roles/role_under_test'
 readonly test_playbook="${role_dir}/docker-tests/test.yml"
 
 readonly docker_image="bertvv/ansible-testing"
+readonly image_tag="${docker_image}:${DISTRIBUTION}_${VERSION}"
 
 # Distribution specific settings
 init="/sbin/init"
@@ -31,53 +52,72 @@ run_opts=("--privileged")
 #}}}
 
 main() {
-  configure_env
+  configure_environment
 
   start_container
 
   run_syntax_check
-  run_playbook
+  run_test_playbook
   run_idempotence_test
 
+  # Uncomment the following line if you want to clean up the container(s) after
+  # running the tests. *Not* cleaning up may be useful for troubleshooting or
+  # running functional tests afterwards.
+
+  # cleanup
 }
 
 #{{{ Helper functions
 
-configure_env() {
+# Usage: configure_environment
+#
+# This function defines run options for Docker, depending on the
+# Linux $DISTRIBUTION and $VERSION.
+configure_environment() {
 
   case "${DISTRIBUTION}_${VERSION}" in
-    'centos_7')
+    centos_6)
+      run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
+      ;;
+    centos_7|fedora_*)
       init=/usr/lib/systemd/systemd
       run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
       ;;
-    'ubuntu_14.04')
+    ubuntu_14.04)
+      #run_opts+=('--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro')
       # Workaround for issue when the host operating system has SELinux
       if [ -x '/usr/sbin/getenforce' ]; then
         run_opts+=('--volume=/sys/fs/selinux:/sys/fs/selinux:ro')
       fi
       ;;
-    'ubuntu_16.04')
+    ubuntu_1[68].04|debian_*)
       run_opts=('--volume=/run' '--volume=/run/lock' '--volume=/tmp' '--volume=/sys/fs/cgroup:/sys/fs/cgroup:ro' '--cap-add=SYS_ADMIN' '--cap-add=SYS_RESOURCE')
 
       if [ -x '/usr/sbin/getenforce' ]; then
         run_opts+=('--volume=/sys/fs/selinux:/sys/fs/selinux:ro')
       fi
       ;;
+    *)
+      warn "Warning: no run options added for ${DISTRIBUTION} ${VERSION}"
+      ;;
   esac
 }
 
 # Usage: build_container
 build_container() {
-  docker build --tag="${docker_image}:${DISTRIBUTION}_${VERSION}" .
+  log "Building container for ${DISTRIBUTION} ${VERSION}"
+  set -x
+  docker build --tag="${image_tag}" .
+  set +x
 }
 
 start_container() {
   log "Starting container"
   set -x
   docker run --detach \
-    --volume="${PWD}:${role_dir}:ro" \
     "${run_opts[@]}" \
-    "${docker_image}:${DISTRIBUTION}_${VERSION}" \
+    --volume="${PWD}:${role_dir}:ro" \
+    "${image_tag}" \
     "${init}" \
     > "${container_id}"
   set +x
@@ -88,15 +128,22 @@ get_container_id() {
 }
 
 # Usage: get_container_ip CONTAINER_ID
+#
+# Prints the IP address of the specified container.
 get_container_ip() {
-  local container_id="${1}"
+  local id
+  id="$(get_container_id)"
 
   docker inspect \
     --format '{{ .NetworkSettings.IPAddress }}' \
-    "${container_id}"
+    "${id}"
 }
 
+# Usage: exec_container COMMAND
+#
+# Run COMMAND on the Docker container
 exec_container() {
+  local id
   id="$(get_container_id)"
 
   set -x
@@ -108,18 +155,19 @@ exec_container() {
 }
 
 run_syntax_check() {
-  log "Running syntax check on playbook"
+  log 'Running syntax check on playbook'
   exec_container ansible-playbook "${test_playbook}" --syntax-check
+  log 'Syntax check finished'
 }
 
-run_playbook() {
-  log "Running playbook"
+run_test_playbook() {
+  log 'Running playbook'
   exec_container ansible-playbook "${test_playbook}"
-  log "Run finished"
+  log 'Run finished'
 }
 
 run_idempotence_test() {
-  log "Running idempotence test"
+  log 'Running idempotence test'
   local output
   output="$(mktemp)"
 
@@ -139,7 +187,8 @@ run_idempotence_test() {
 }
 
 cleanup() {
-  log "Cleaning up"
+  log 'Cleaning up'
+  local id
   id="$(get_container_id)"
 
   docker stop "${id}"
@@ -147,37 +196,22 @@ cleanup() {
   rm "${container_id}"
 }
 
-# Check if command line arguments are valid
-check_args() {
-  if [ "${#}" -ne "1" ]; then
-    echo "Expected 1 argument, got ${#}" >&2
-    usage
-    exit 2
-  fi
-}
-
-# Print usage message on stdout
-usage() {
-cat << _EOF_
-Usage: ${script_name} [OPTIONS]... [ARGS]...
-
-  Creates a Docker container, installs this role, and runs tests.
-
-OPTIONS:
-
-EXAMPLES:
-_EOF_
-}
-
+# Usage: log [ARG]...
+#
+# Prints all arguments on the standard output stream
 log() {
-  local yellow='\e[0;33m'
-  local reset='\e[0m'
-
-  printf "${yellow}>>> %s${reset}\n" "${*}"
+  printf "${yellow}>>> %s${reset}\\n" "${*}"
 }
+
+# Usage: warn [ARG]...
+#
+# Prints all arguments on the standard error stream
+warn() {
+  printf "${red}!!! %s${reset}\\n" "${*}" 1>&2
+}
+
 
 #}}}
 
 main "${@}"
 
-#trap cleanup EXIT INT ERR HUP TERM
